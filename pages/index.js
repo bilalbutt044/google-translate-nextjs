f9e2887a8bd5e740deca0d0f50b38b9ad8553ec3
Muhammad Bilal <bilal.soft000@gmail.com>
2023-11-24 18:59:46 +0500

    error fix

--- pages/index.js
+++ pages/index.js
@@ -35,8 +35,6 @@ export default function Home() {
           name="twitter:image"
           content="https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         />
-
-        <meta name="robots" content={`${index}, ${follow}`} />
       </Head>
       <main className="flex flex-col items-center justify-between min-h-screen p-24">
         <div
