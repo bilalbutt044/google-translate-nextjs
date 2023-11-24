0f7f0a31bc2c1e617dda19229b48690b2715fe47
Muhammad Bilal <bilal.soft000@gmail.com>
2023-11-24 19:08:39 +0500

    not rendering on server side

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import "../globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  if (typeof window === "undefined") {
+    return <></>;
+  } else return <Component {...pageProps} />;
 }
